b594d5e6f66bb08241a0cdbad6451ca1775e6eee
Shilton <shilton.sistemas@gmail.com>
2020-05-03 14:24:52 -0300

    Configuring ESLint, Prettier and EditorConfig

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, //uso de aspas simples
+  trailingComma: 'all', //virgula no final de objs e arrays
+  allowParens: 'avoid' //não colocar () em arrow function com unico parametro
+}

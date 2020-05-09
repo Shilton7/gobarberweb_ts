ee2995e806f6db8f1f22eab1728605be4d8f6fe5
Shilton <shilton.sistemas@gmail.com>
2020-05-09 20:10:06 -0300

    Application logout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SigIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>

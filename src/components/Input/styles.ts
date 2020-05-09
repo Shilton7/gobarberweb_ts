6bec5fe06c1509105e5a753bc0538ee318acc39a
Shilton <shilton.sistemas@gmail.com>
2020-05-09 11:08:34 -0300

    Unform and usability of the input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   width: 100%;
   padding: 16px;
   color: #666360;
@@ -13,6 +18,22 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  /* Focus input selected */
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  /* Blur input */
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
 
   input {
     flex: 1;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/img/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="Gobarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
@@ -25,7 +30,7 @@ const SignUp: React.FC = () => (
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="login">
           <FiArrowLeft size={20} />
@@ -33,7 +38,7 @@ const SignUp: React.FC = () => (
         </a>
       </Content>
     </Container>
-  </>
-);
+  );
+};
 
 export default SignUp;
